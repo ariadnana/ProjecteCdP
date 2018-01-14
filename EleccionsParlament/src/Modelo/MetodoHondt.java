64433a3afa1b18ab587860a2c37f021737fa0b57
bertacumellas <bertacume@gmail.com>
2018-01-14 19:49:05 +0100

    Canvi en el metode de Hondt

--- EleccionsParlament/src/Modelo/MetodoHondt.java
+++ EleccionsParlament/src/Modelo/MetodoHondt.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 
 public class MetodoHondt implements MetodoCalculo{
 	
@@ -30,24 +31,49 @@ public class MetodoHondt implements MetodoCalculo{
 		for(int i = 1; i <= totalescanos; i++) {
 			double num = 0;
 			String partido = "";
+			List<String> igualVotos = new ArrayList<String>();//Només utilitzat quan coincideixen els quocients i els numeros de vots totals
 			for(Map.Entry<String, List<Double>> listavotos: tablaVotos.entrySet()){
 				for(double valor: listavotos.getValue()) {
 					if (valor == num && partido != "") {
 							if(escr.get(listavotos.getKey()) > escr.get(partido)) {
 								num = valor;	
 								partido = listavotos.getKey();
-								//TODO: si hi haguessin els mateix vots s'haurien d'alternar els escons 
-						}
+								igualVotos.clear();
+							}else if(escr.get(listavotos.getKey()) == escr.get(partido)) {
+								//si hi haguessin els mateix vots s'haurien d'alternar els escons 
+								if(igualVotos.size() == 0) {
+									igualVotos.add(partido);
+								}
+								igualVotos.add(listavotos.getKey());		
+								num = valor;
+								System.out.println("mateix " + num);
+								System.out.println("llista repes " + igualVotos);
+							}
 					}else if(valor > num) {
 						num = valor;	
 						partido = listavotos.getKey();
+						igualVotos.clear();
 					}
 				}
 			}
 			if(!partido.equals("")) {
-				int index = tablaVotos.get(partido).indexOf(num);
-				tablaVotos.get(partido).remove(index);
-				escanos.put(partido, escanos.get(partido) + 1);
+				if(igualVotos.size() > 0) {
+					//Cas en que els nums coincideixen i tmb els numeros de vots totals
+					Random rand = new Random();
+					for(int j = 0; (j < igualVotos.size()) && ((totalescanos - i + 1) > 0); j++) {
+						int x = rand.nextInt(igualVotos.size());
+						escanos.put(igualVotos.get(x), escanos.get(partido) + 1);
+						System.out.println("eee" + igualVotos.get(x));
+						int index = tablaVotos.get(igualVotos.get(x)).indexOf(num);
+						tablaVotos.get(igualVotos.get(x)).remove(index);
+						igualVotos.remove(x);
+						totalescanos -= 1;
+					}
+				}else {
+					int index = tablaVotos.get(partido).indexOf(num);
+					tablaVotos.get(partido).remove(index);
+					escanos.put(partido, escanos.get(partido) + 1);
+				}
 			}
 		}
 		return escanos.entrySet();
